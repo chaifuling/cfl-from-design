a144e3adedd730f69fbba4b4a0e4017621d705d0
caifuling <448527381@qq.com>
2023-05-16 14:58:13 +0800

    feat:完善组件功能

--- src/components/generator/drawingDefalut.js
+++ src/components/generator/drawingDefalut.js
@@ -1,37 +1,36 @@
-import {ref} from 'vue'
+import { tFn } from '@/hook/useI18n';
 
 export default [
-  {
+ {
+    // 组件的自定义配置
     __config__: {
-      label: '单行文本',
+      label: tFn('tag.input.label'),
       showLabel: true,
       changeTag: true,
       tag: 'a-input',
       tagIcon: 'inputIcon',
-      defaultValue: '请输入',
+      defaultValue: undefined,
       required: true,
       layout: 'colFormItem',
       span: 24,
-
+      document: 'tag.input.document',
       // 正则校验规则
-      regList: [{
-        pattern: '/^1(3|4|5|7|8|9)\\d{9}$/',
-        message: '手机号格式错误'
-      }]
+      regList: []
     },
     // 组件的插槽属性
     __slot__: {
       addonBefore: '',
       addonAfter: ''
     },
-    __vModel__: 'mobile',
-    defaultValue: '请输入',
+    // 其余的为可直接写在组件标签上的属性
+    // 'default-value': 'aaaa',
+    placeholder: tFn('tag.input.label'),
     style: { width: '100%' },
     allowClear: true,
     maxLength: null,
     disabled: false,
     size: 'default',
-    prefix:'',
-    suffix: '',
+    prefix: '',
+    suffix: ''
   },
 ]
